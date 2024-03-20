19da18b165c5d7c71eb87cfbf37ef52f315af464
fatihfdan <105379228+fatihfdan@users.noreply.github.com>
2024-03-20 06:42:01 +0300

    Movies App Example
    Fetch

---
+++ src/components/Movie.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+
+function Movie() {
+  const [movieList, setMovieList] = useState([]);
+  const getMovie = () => {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?api_key=1e31e940b34b9de92c3a0afe8f933b38"
+    )
+      .then((res) => res.json())
+      .then((json) => setMovieList(json.results));
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  console.log(movieList);
+
+  return (
+    <div>
+      {movieList.map((movie) => (
+        <img
+          style={{
+            width: "300px",
+            height: "250px",
+            marginLeft: "10px",
+            marginTop: "10px",
+          }}
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Movie;
