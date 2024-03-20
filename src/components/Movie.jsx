fd23e18755c684e4e3f1d4482cf211fc5d492c69
fatihfdan <105379228+fatihfdan@users.noreply.github.com>
2024-03-20 06:51:33 +0300

    Movies App Example
    Movies App Example

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -2,19 +2,36 @@ import React, { useEffect, useState } from "react";
 
 function Movie() {
   const [movieList, setMovieList] = useState([]);
-  const getMovie = () => {
-    fetch(
-      "https://api.themoviedb.org/3/discover/movie?api_key=1e31e940b34b9de92c3a0afe8f933b38"
-    )
-      .then((res) => res.json())
-      .then((json) => setMovieList(json.results));
+  //   const getMovie = () => {
+  //     fetch(
+  //       "https://api.themoviedb.org/3/discover/movie?api_key=1e31e940b34b9de92c3a0afe8f933b38"
+  //     )
+  //       .then((res) => res.json())
+  //       .then((json) => setMovieList(json.results));
+  //   };
+
+  //   useEffect(() => {
+  //     getMovie();
+  //   }, []);
+
+  //   console.log(movieList);
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZTMxZTk0MGIzNGI5ZGU5MmMzYTBhZmU4ZjkzM2IzOCIsInN1YiI6IjY1ZmEyOWQ1ZmZjOWRlMDE0YTVlYzllMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QcBSwSwS7IrqDrUyM7cIjiF5Gp5lyveIHSe_K-aBL1g",
+    },
   };
 
-  useEffect(() => {
-    getMovie();
-  }, []);
-
-  console.log(movieList);
+  fetch(
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=1&sort_by=popularity.desc&api_key=1e31e940b34b9de92c3a0afe8f933b38",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => setMovieList(response.results))
+    .catch((err) => console.error(err));
 
   return (
     <div>
